f55dd6f267afb67e9d32508fa6dad17751c0468d
Leandro Andrade <leandro.andrade2588@gmail.com>
2022-10-20 09:26:32 -0300

    feat: add test swagger route

---
+++ tests/integration/swagger.test.js
@@ -0,0 +1,16 @@
+import t from 'tap';
+import { buildApp } from '../shared/helper.js';
+
+const { test } = t;
+
+test('should return sample response', async t => {
+    const fastify = buildApp(t);
+
+    const response = await fastify.inject({
+        method: 'GET',
+        url: '/docs',
+    });
+
+    t.equal(response.statusCode, 302);
+    t.equal(typeof response.payload, 'string');
+});
