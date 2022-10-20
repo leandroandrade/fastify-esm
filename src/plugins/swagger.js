f530ccd58938a0aef1e7f2427b5a4008e203460f
Leandro Andrade <leandro.andrade2588@gmail.com>
2022-10-20 09:34:24 -0300

    feat: update dependencies and improve tests

--- src/plugins/swagger.js
+++ src/plugins/swagger.js
@@ -1,18 +1,23 @@
-import fp from 'fastify-plugin';
-import swagger from '@fastify/swagger';
 import { fileURLToPath } from 'node:url';
 import { join, dirname } from 'node:path';
+import fp from 'fastify-plugin';
+import swagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 async function swaggerPlugin(fastify, opts) {
     fastify.register(swagger, {
-        routePrefix: '/docs',
         exposeRoute: true,
         mode: 'static',
         specification: {
             path: join(__dirname, '..', 'swagger', 'sample-swagger.yaml'),
         },
+    });
+
+    fastify.register(fastifySwaggerUi, {
+        routePrefix: '/docs',
+        logLevel: 'silent',
     });
 }
 
