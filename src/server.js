d4dc1dbfbd4a39579b287a2b391cf12cb391eb56
Leandro Andrade <leandro.andrade2588@gmail.com>
2022-10-27 20:21:18 -0300

    feat: add close with grace

--- src/server.js
+++ src/server.js
@@ -1,26 +1,25 @@
+import closeWithGrace from 'close-with-grace';
 import buildApp from './app.js';
 
 const app = buildApp();
 const port = process.env.PORT || 8080;
 
-app.listen(port, '0.0.0.0')
+app.listen({ port, host: '0.0.0.0' })
     .then(() => console.log(`App running at ${port}`))
     .catch(err => console.error(err));
 
-['SIGTERM', 'SIGINT'].forEach(signal => {
-    process.on(signal, async () => {
-        try {
-            console.info(`${signal} signal received. Closing application...`);
+closeWithGrace({ delay: 10000 }, async ({ signal }) => {
+    try {
+        console.info(`${signal} signal received. Closing application...`);
 
-            console.info('Closing HTTP server...');
-            await app.close();
-            console.info('HTTP server closed!');
+        console.info('Closing HTTP server...');
+        await app.close();
+        console.info('HTTP server closed!');
 
-            console.info('Application closed successfuly!');
-            process.exit(0);
-        } catch (err) {
-            console.error(`Application exited with error: ${err}`);
-            process.exit(1);
-        }
-    });
+        console.info('Application closed successfuly!');
+        process.exit(0);
+    } catch (err) {
+        console.error(`Application exited with error: ${err}`);
+        process.exit(1);
+    }
 });
